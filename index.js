9cb3f9269adde1437cad70af784d810a54ab28aa
ThousandDucks <edison03lee@gmail.com>
2026-01-20 20:03:48 +0000

    Added functions to create rows and columns. Added borders to the rows and columns.

---
+++ index.js
@@ -0,0 +1,31 @@
+
+const canvas = document.getElementById("canvas")
+const rows = document.querySelectorAll(".row")
+
+
+
+function createRow(rowNum) {
+    for (let i=0; i<rowNum; i++) {
+        let row = document.createElement("div")
+         row.classList.add("row")
+         canvas.appendChild(row)
+    }
+}
+
+function createCol(colNum) {
+    const rows = document.querySelectorAll(".row");
+    rows.forEach(row => {
+        for (let j =0; j<colNum; j++) {
+            let col = document.createElement("div")
+            col.classList.add("col")
+            row.append(col)
+        }
+    })
+}
+
+function createCells(rowNum, colNum) {
+    createRow(rowNum)
+    createCol(colNum)
+}
+
+createCells(3,3)
