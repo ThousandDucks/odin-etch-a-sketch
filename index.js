bb7dade98b389c4c145260222f59a2cebc691ebe
ThousandDucks <edison03lee@gmail.com>
2026-01-21 18:47:15 +0000

    Added colour palette to allow for different colours on the grid

--- index.js
+++ index.js
@@ -30,6 +30,7 @@ function createCells(rowNum, colNum) {
 createCells(16,16)
 
 // Allow for colouring of grid
+let currentColor = "black";
 let mouseDown = false;
 
 canvas.addEventListener("dragstart", (event) => {
@@ -46,12 +47,20 @@ document.addEventListener("mouseup", () => {
 
 canvas.addEventListener("mouseover", (event) => {
     if (mouseDown && event.target.classList.contains("col")) {
-        event.target.style.backgroundColor = "blue";
+        event.target.style.backgroundColor = currentColor;
     }
+});
+
+// Allows for single clicks
+canvas.addEventListener("mousedown", (event) => {
+    event.target.style.backgroundColor = currentColor;
 });
 
 // Resizing and resetting grid
 const resizeButton = document.querySelector("#resizeButton")
+const resetButton = document.querySelector("#resetButton")
+
+let gridSize = 16;
 
 resizeButton.addEventListener("click", () => {
     let resizePrompt = Number(prompt("Enter the number of squares per side for the new grid (max 100):"))
@@ -59,7 +68,23 @@ resizeButton.addEventListener("click", () => {
     if (resizePrompt > 100 || resizePrompt < 1) {
         return;
     } else {
+        gridSize = resizePrompt;
         canvas.innerHTML = "";
         createCells(resizePrompt, resizePrompt)
     }
+})
+
+resetButton.addEventListener("click", () => {
+    canvas.innerHTML = "";
+    console.log(gridSize)
+    createCells(gridSize, gridSize)
+})
+
+// Add colouring option to buttons
+const colourButton = document.querySelectorAll(".colourBtn")
+
+colourButton.forEach(button => {
+    button.addEventListener("click", () => {
+        currentColor = button.style.backgroundColor;
+    })
 })
